0a375fbf11a11bba884deb817827cbb7a5e81091
Christopher Johnson <36961199+critterjohnson@users.noreply.github.com>
2020-02-24 11:44:09 -0500

    more tests

--- sevenbyfour.py
+++ sevenbyfour.py
@@ -1,6 +1,7 @@
 # sevenbyfour.py - control 4 seven segment displays
 from sevensegment import SevenSegment
 from shiftregister import ShiftRegister
+import RPi.GPIO as GPIO
 
 sel1 = 0
 sel2 = 0
@@ -15,4 +16,7 @@ displays = [SevenSegment(shift),
 # digits should be a list of digits to display, dps should be a list of bools
 def disp(digits, dps):
     for i, display in enumerate(displays):
+        binarysel = bin(i)[2:]
+        GPIO.output(sel1, binarysel[0])
+        GPIO.output(sel2, binarysel[1])
         display.display(digits[i], dps[i])
